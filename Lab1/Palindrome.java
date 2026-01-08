38824dbd33a9c7f07f64d2c9d80c3cdb8aecf050
2405455-sketch <2405455@kiit.ac.in>
2026-01-08 14:27:45 +0530

    remaining day 1 assignments

---
+++ Lab1/Palindrome.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Palindrome {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        int temp = num, rev = 0;
+        while (temp > 0) {
+            rev = rev * 10 + (temp % 10);
+            temp /= 10;
+        }
+
+        if (num == rev)
+            System.out.println("Number is Palindrome");
+        else
+            System.out.println("Number is not Palindrome");
+
+        sc.close();
+    }
+}
